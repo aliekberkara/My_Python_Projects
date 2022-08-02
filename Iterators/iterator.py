60c2eea6cfd6cb58a11a4347946316ed57f38664
Ali Ekber KARA <aliekber123kara@gmail.com>
2022-08-02 17:38:00 +0300

    Create iterator.py

---
+++ Iterators/iterator.py
@@ -0,0 +1,19 @@
+class My_Numbers:
+    def __init__(self, start, stop):
+        self.start = start
+        self.stop = stop
+    
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.start <= self.stop:
+            x = self.start
+            self.start += 1
+            return x
+        else:
+            raise StopIteration
+
+list = My_Numbers(5, 15)
+
+for x in list:
+    print(x)
